ce8f13c33bbc34855ddb5ef520dd74b1b6c3300a
会飞的老虎 <@DESKTOP-KVUVU2M>
2019-05-21 16:20:03 +0800

    添加项目文件。

---
+++ Controllers/IndexController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationPlatform.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Platform()
+        {
+            return View();
+        }
+
+        public ActionResult Homepage()
+        {
+            return View();
+        }
+        public ActionResult Abc()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationPlatform.MvcApplication" Language="C#" %>
